c885ed55182c2fe90bb4b647683326a2f814d335
Super-King-Crimson <129329889+Super-King-Crimson@users.noreply.github.com>
2023-08-09 01:10:39 -0400

    fin

--- src/main.rs
+++ src/main.rs
@@ -8,10 +8,3 @@ fn main() {
     trait_objects::explain();
     oop_design_patterns::explain();
 }
-
-fn concat_all<'a>(
-    iter: impl Iterator<Item = String> + 'a,
-    s: &'a str
-) -> impl Iterator<Item = String> + 'a {
-    iter.map(move |s2| s2 + s)
-}
